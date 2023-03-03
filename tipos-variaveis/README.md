6fafc5e44fbfd6fb611ec4edb4223b5e9df383bb
Talita Rocha <tali_talitarocha@hotmail.com>
2023-03-02 23:02:55 -0300

    Operadores ternários

--- tipos-variaveis/README.md
+++ tipos-variaveis/README.md
@@ -4,4 +4,4 @@ Este é um repositório de testes e exercícios das aulas do curso de Java Bási
 
 ## Conteúdo
 
-O projeto foi nomeado inicialmente como "tipos-variaveis", no entanto, ao longo do curso foram abordados também assuntos relacionados a constantes, tipos de dados e de variáveis, operadores etc. 
+O projeto foi nomeado inicialmente como "tipos-variaveis", no entanto, ao longo do curso foram abordados também assuntos relacionados a constantes, tipos de dados e de variáveis, operadores, métodos, escopo, palavras reservadas, terminal e argumentos.
---
+++ tipos-variaveis/src/OperadoresTernarios.java
@@ -0,0 +1,27 @@
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+
+         
+        int a, b;
+        a = 5;
+        b = 6;
+        /*
+        String resultado = "";
+
+        // Exemplo de condicional utilizando uma estrutura if/else
+        if (a == b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        System.out.println(resultado);
+        
+        Há uma forma ainda mais abreviada de tomar essa decisão: 
+        */
+String resultado = a==b ? "verdadeiro" : "falso";
+
+System.out.println(resultado);
+
+    }
+
+}
