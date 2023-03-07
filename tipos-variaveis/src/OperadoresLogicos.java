b57e50101494c8dab32493bce67d182f5b118bec
Talita Rocha <tali_talitarocha@hotmail.com>
2023-03-07 13:57:04 -0300

    Anotações sobre operadores lógicos

---
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -0,0 +1,24 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condições são verdadeiras");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das duas condições é verdadeira");
+        }
+
+        if (condicao1 && (5 > 3)){
+            System.out.println("As duas condiões são verdadeiras");
+        }
+
+        System.out.println("Fim!");
+    }
+}
