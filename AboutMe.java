852880c977d0db5f0f6f624fb8e5b803e2906119
Talita <89655554+ttp3r@users.noreply.github.com>
2023-03-09 16:25:08 -0300

    Anotações sobre Parâmetros

---
+++ AboutMe.java
@@ -0,0 +1,18 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        // os argumentos começam com indíce 0
+
+        String nome = args[0];
+        String sobrenome = args[1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+        //A partir de agora, este programa sempre exigirá parâmetros
+
+        
+
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + " cm");
+
+    }
+}
